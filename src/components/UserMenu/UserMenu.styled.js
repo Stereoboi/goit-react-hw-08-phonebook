1d4e3876f38cb260f0acb2dfc647a37f35606e4a
Bohdan Pishchak <3.14shcha@gmail.com>
2022-11-19 13:11:41 +0200

    main

--- src/components/UserMenu/UserMenu.styled.js
+++ src/components/UserMenu/UserMenu.styled.js
@@ -1,18 +1,5 @@
 import styled from "styled-components";
 
-
-// export const Wrapper = styled.div`
-//   display: flex;
-//   align-items: center;
-//   gap: 12px;
-// `
-
-// export const Text = styled.div`
-//   display: flex;
-//   align-items: center;
-//   gap: 12px;
-// `
-
 export const UserName = styled.p`
   font-family: "Roboto","Helvetica","Arial",sans-serif;
   font-weight:400;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-// import { LoginForm } from "components/LoginForm/LoginForm"
+
 import SignIn from "components/LoginForm/LoginForm"
 
 export default function Login()  {
